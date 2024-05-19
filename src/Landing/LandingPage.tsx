05c6fd5b701f9943f29408fe6d9d2312a202bdf6
AkionAG07 <akion.cheng.jimenez@est.una.ac.cr>
2024-05-19 15:47:08 -0600

    Header, Start of LandingPage and build de rutes on the main

---
+++ src/Landing/LandingPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import HeaderLading from './HeaderLading'
+import OptionsHeader from './OptionsHeader'
+
+function LandingPage() {
+  return (
+    <>
+      <HeaderLading />
+      <OptionsHeader />
+    </>
+  )
+}
+
+export default LandingPage
