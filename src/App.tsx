8be202e843b285012ea2a516b37b92982a3efb64
Dian <dian7875.a@gmail.com>
2024-05-19 18:47:52 -0600

    Add Login and Registrer forms. dark mode test v2

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,7 @@
-// src/App.tsx
-import React from 'react';
-import useDarkMode from './Hooks/useDarkMode';
-import DarkModeToggle from './components/DarkModeButton';
 
 const App: React.FC = () => {
-  const { isDarkMode, toggleDarkMode } = useDarkMode();
   return (
     <>
-      <div className={`min-h-screen ${isDarkMode ? 'dark bg-gray-900 text-white' : 'bg-white text-black'}`}>
-        <DarkModeToggle isDarkMode={isDarkMode} toggleDarkMode={toggleDarkMode} />
-      </div>
     </>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import ThemeProvider from './assets/ThemeContext/ThemeProvider.tsx'
+import GeneralProvider from './assets/GeneralContext/GeneralProvider.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider>
+    <GeneralProvider>
+    <App/>
+    </GeneralProvider>
+    </ThemeProvider>
   </React.StrictMode>,
 )
