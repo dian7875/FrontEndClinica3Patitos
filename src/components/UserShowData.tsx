1eecf060c117b1af1ed12544345e6d05c2acc3f2
AkionAG07 <akion.cheng.jimenez@est.una.ac.cr>
2024-05-21 14:54:28 -0600

    card del usuario

--- src/components/UserShowData.tsx
+++ src/components/UserShowData.tsx
@@ -5,26 +5,28 @@ function UserShowData() {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleInfo = () => {
-    console.log("Estado antes de cambiar:", isOpen);
     setIsOpen(!isOpen);
-    console.log("Estado después de cambiar:", !isOpen);
   };
 
   return (
-    <div className="p-4">
-      <h1
-        className="text-2xl font-bold cursor-pointer"
-        onClick={toggleInfo}
-      >
-        Nombre
-      </h1>
-      {isOpen ?  (
-        <div className="mt-2">
-          <p>Correo: ejemplo@correo.com</p>
-          <p>Teléfono: 123-456-7890</p>
-        </div>
-      ): null}
+    <div className="flex flex-col  gap-3 p-4">
+  <div onClick={toggleInfo} className="flex gap-3">
+    <img className='w-8 h-8' src="https://cdn-icons-png.flaticon.com/128/3033/3033143.png" alt="" />
+    <h1
+      className="text-2xl font-bold cursor-pointer"
+      
+    >
+      Nombre
+    </h1>
+  </div>
+  {isOpen ? (
+    <div >
+      <p>Correo: ejemplo@correo.com</p>
+      <p>Teléfono: 123-456-7890</p>
     </div>
+  ) : null}
+</div>
+    
   );
 
 
