a41f4be7a4498ad4dbabcf223893dd9ed1178899
Dian7875 <dian7875.a@gmail.com>
2024-05-22 04:18:36 -0600

    change Label and test new gitbranch

--- src/components/AppoinmentsComponents/FormReservation.tsx
+++ src/components/AppoinmentsComponents/FormReservation.tsx
@@ -10,7 +10,7 @@ function FormReservation() {
        h-1/5 px-3 my-16 mx-48 rounded-2xl text-lg">
         <form className="flex justify-between gap-3 h-3/5">
           <div className="flex flex-col w-full">
-            <span className="">Fecha</span>
+            <label className="">Fecha</label>
             <input
               className="text-gray-500 border rounded-md h-full border-gray-500"
               type="datetime-local"
@@ -18,7 +18,7 @@ function FormReservation() {
             />
           </div>
           <div className="flex flex-col w-full">
-            <span className="">Sucursal</span>
+            <label className="">Sucursal</label>
             <select
               className="text-gray-500 border rounded-md h-full border-gray-500"
               {...register("Branch_Name")}>
@@ -28,7 +28,7 @@ function FormReservation() {
             </select>
           </div>
           <div className="flex flex-col w-full">
-            <span className="">Tipo</span>
+            <label className="">Tipo</label>
             <select
               className="text-gray-500 border rounded-md h-full border-gray-500"
               {...register("Name_Type")}>
