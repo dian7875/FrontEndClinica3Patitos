dba5738f082138a76e9cd5290650854c1e5b6025
AkionAG07 <akion.cheng.jimenez@est.una.ac.cr>
2024-05-21 23:26:14 -0600

    AppoimentGeneral page done

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,9 +1,12 @@
-import     {  useState } from "react";
+
 import { ButtonEdit, ButtonCancelar, ButtonEliminar } from "./ButtonCards";
 import UseGetAllAppoiments from "../hooks/UseGetAllAppoiments";
+import {useForm} from 'react-hook-form'; 
+
 function Card() {
   const { appoiments } = UseGetAllAppoiments();
-  const [inputValue, setInputValue] = useState<string>();
+  
+  const {register, handleSubmit, formState: {errors}} = useForm();
 
   return (
     <>
@@ -16,21 +19,19 @@ function Card() {
             <div className="grid grid-cols-3 mt-4 gap-1">
               <label className="text-xs">Fecha</label>
               <label className="text-xs">Sucursal</label>
-              <label className="text-xs">Tipo</label>
+              <label className="text-xs">Status</label>
             </div>
             <div className="grid grid-cols-3 gap-2">
               <input
                 className="text-xs text-gray-500 border rounded-md border-gray-500"
                 type="date"
                 value="2021-09-01"
-                onChange={(e) => setInputValue(e.target.value)}
+                {...register("Date")}
               />
               <select
                 className="text-xs  text-gray-500 border rounded-md border-gray-500"
-                name=""
                 id=""
-                value={appoiment.Branch_Name}
-                onChange={(e) => {}}
+               {...register("Branch_Name")}
               >
                 <option value="">{appoiment.Branch_Name}</option>
                 <option value="">3 Patitos Nicoya</option>
