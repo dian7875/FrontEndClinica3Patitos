0cc4ad43131f91fac739d9720df4f389a172331c
Dian7875 <dian7875.a@gmail.com>
2024-05-22 21:04:29 -0600

    Fix General App form

--- src/components/AppoinmentsComponents/Card.tsx
+++ src/components/AppoinmentsComponents/Card.tsx
@@ -11,50 +11,48 @@ function Card() {
 
   return (
     <>
-      {appoiments.map((appoiment) => {
-        return (
-          <form
-            key={appoiment.id}
-            className="bg-white w-full h-full rounded-lg">
-            <div className="flex w-full gap-4">
-              <div className="flex flex-col w-full">
-                <label className="text-xs">Fecha</label>
-                <input title="a"
-                  className="text-xs text-gray-500 border rounded-md border-gray-500"
-                  type="datetime-local"
-                  {...register("Date")} />
-              </div>
-              <div className="flex flex-col w-full">
-                <label className="text-xs">Branch</label>
-                <select title="a"
-                  className="text-xs  text-gray-500 border rounded-md border-gray-500"
-                  id=""
-                  value={appoiment.Branch_Name}
-                  onChange={() => { }}
-                >
-                  <option value="">{appoiment.Branch_Name}</option>
-                  <option value="">3 Patitos Nicoya</option>
-                  <option value="">3 Patitos San Martin</option>
-                  <option value="">3 Patitos Desamparados</option>
-                </select>
-              </div>
-              <div className="flex flex-col w-full">
 
-              <label className="text-xs">Status</label>
-              <label
-                className="text-xs  text-gray-500 border rounded-md border-gray-500"
-                htmlFor=""
-                >
-                {appoiment.Status ? "Activo" : "Cancelado"}
-              </label>
-                  </div>
-            </div>
-            <div className="flex justify-around">
-              <ButtonCards />
-            </div>
-          </form>
-        );
-      })}
+      <div className={`grid grid-cols-2 gap-5 `}>
+        {appoiments.map((appoiment) => {
+          return (
+            <form
+              key={appoiment.id}
+              className="bg-white rounded-lg h-32 px-2 pt-5 relative shadow-xl">
+              <span className="absolute" style={{ top: '-5px' }}>Pacient Name: {appoiment.Past_Name}</span>
+              <div className="flex w-full h-3/5 gap-4">
+                <div className="flex flex-col w-full">
+                  <label className="">Date</label>
+                  <input title=""
+                    className="h-full text-gray-500 border rounded-md border-gray-500"
+                    type="datetime-local"
+                    {...register("Date")} />
+                </div>
+                <div className="flex flex-col w-full">
+                  <label className="">Branch</label>
+                  <select title="a"
+                    className=" text-gray-500 border h-full rounded-md border-gray-500"
+                    id=""
+                    value={appoiment.Branch_Name}>
+                    <option value="">{appoiment.Branch_Name}</option>
+                    <option value="">3 Patitos Nicoya</option>
+                    <option value="">3 Patitos San Martin</option>
+                    <option value="">3 Patitos Desamparados</option>
+                  </select>
+                </div>
+                <div className="flex flex-col w-full">
+                  <label className="">Status</label>
+                  {appoiment.Status ? <span className="border text-center pt-2 h-full rounded-md font-serif bg-white ">Pendiente</span> 
+                  :
+                   <span className="border text-center pt-2 h-full rounded-md bg-primary text-white ">Cancelada</span>}
+                </div>
+              </div>
+              <div className="flex justify-around h-8 pt-1">
+                <ButtonCards />
+              </div>
+            </form>
+          );
+        })}
+      </div>
     </>
   );
 }
--- src/pages/GeneralAppo.tsx
+++ src/pages/GeneralAppo.tsx
@@ -1,17 +1,14 @@
 
-import { useState } from "react"
 import AppoimentsContainer from "../components/AppoinmentsComponents/AppoimentsContainer"
 import FormReservation from "../components/AppoinmentsComponents/FormReservation"
 
 const GeneralAppo = () => {
-  const [isExpanded, setIsExpanded] = useState<boolean>(false)
   return (
     <>
-    <main className={`flex flex-col pt-12 pb-12 h-screen
-    ${!isExpanded ? `overflow-hidden`:`overflow-auto`} `}>
-      <FormReservation/>
-      <AppoimentsContainer isExpanded={isExpanded} setIsExpanded={setIsExpanded} />
-    </main>
+      <main className="h-screen flex flex-col pt-12 pb-16">
+          <FormReservation />
+          <AppoimentsContainer />
+      </main>
     </>
   )
 }
