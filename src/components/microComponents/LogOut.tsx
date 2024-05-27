03a070316dd0f71446d56dc46996b2958cba08d7
AkionAG07 <akion.cheng.jimenez@est.una.ac.cr>
2024-05-26 19:09:12 -0600

    poner el span a desloguear

--- src/components/microComponents/LogOut.tsx
+++ src/components/microComponents/LogOut.tsx
@@ -14,7 +14,7 @@ const LogOut = () => {
     <>
     <div className={`shadow-lg    mt-2  gap-2 flex w-44   rounded-r-md ${isDarkMode ? `dark shadow-Pop-darkborder bg-Pop-darkborder border border-Pop-darkbord ` : `shadow-Pop-bordes bg-Pop-bordes border`}`}>
       <button onClick={onLogOut} type="button"><img className={`w-8 ml-3 mt-1 mb-1 ${isDarkMode ? `dark invert` : ``}`} src="https://cdn-icons-png.flaticon.com/128/660/660350.png" alt="" />  </button>
-      <span className="mt-2">Log Out</span>
+      <span  onClick={onLogOut} className="mt-2">Log Out</span>
       </div>
     </>
   )
